0cee0f39832d92f34b7c0a3dcdadcc122c6a1fd3
hudashe <143215991+hudashe@users.noreply.github.com>
2023-09-18 14:14:54 -0400

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,13 +1,13 @@
 <?php
     
-    $inData = getRequestInfo();
+    	$inData = getRequestInfo();
 
-  $firstName = $inData["firstName"];
+  	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
-  $userId = $inData["userId"];
+  	$userId = $inData["userId"];
 	$login = $inData["login"];
 	$password = $inData["password"];
-  $phone = $inData["phone"];
+  	$phone = $inData["phone"];
 	$email = $inData["email"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
@@ -16,23 +16,24 @@
 		returnWithError( $conn->connect_error );
 	} 
 	else {
-    $stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Email = ?, Phone = ? WHERE ID = ?");
-    $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $userId);
-    $stmt->execute();
+    		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Email = ?, Phone = ? WHERE ID = ?");
+    		$stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $userId);
+    		$stmt->execute();
 
-    if ($result->num_rows > 0) {
-        returnWithError("");
-    }
+    	if ($result->num_rows > 0) {
+        	returnWithError("");
+    	}
 
-    else {
-        returnWithError("Error in Finding Contact.");
-    }
+    	else {
+                returnWithError("Error in Finding Contact.");
+    	}
 
-    $stmt->close();
-    $conn->close();
-}
+    	$stmt->close();
+    	$conn->close();
+	
+        }
 
-function getRequestInfo()
+	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
